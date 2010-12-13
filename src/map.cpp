5d751914ea09437aae8c48269785c621c31608df
Heiko Thiel <heiko.thiel@student.hpi.uni-potsdam.de>
2010-12-13 10:33:52 +0100

    started to implement new ini format

--- src/map.cpp
+++ src/map.cpp
@@ -1,13 +1,16 @@
+#include "map.h"
 #include <string>
 #include <sstream>
 #include <SimpleIni.h>
-#include "map.h"
+#include <osg/Texture2D>
+#include <osgDB/ReadFile>
 
 Map::Map(const std::string& filename)
 {
 	Ini.LoadFile(filename.c_str());
 
 	loadMap();
+	loadTextures();
 }
 
 unsigned int Map::getWidth()
@@ -38,7 +41,37 @@ void Map::loadMap()
 		//allow up to 999.999 rows
 		char rowIdx[10];
 		sprintf_s(rowIdx, 10, "Row%d", y+1);
-		Fields[y].assign(Ini.GetValue("Fields", rowIdx, ""));
+		Fields[y].assign(Ini.GetValue("Map", rowIdx, ""));
 		Fields[y].resize(Width, INI_FIELD_GRAS);
+	}
+}
+
+void Map::loadTextures()
+{
+	std::string sad = Ini.GetValue("Textures", "Tex0", "sd");
+	int textureCount = Ini.GetSectionSize("Textures");
+	if (textureCount == -1)
+	{
+		textureCount = 0;
+	}
+
+	Textures.resize(textureCount);
+	for (int i = 0; i < textureCount; i++)
+	{
+		char texIdx[10];
+		sprintf_s(texIdx, 10, "Tex%d", i+1);
+		std::string tex = "textures/";
+		tex.append(Ini.GetValue("Textures", texIdx, ""));
+
+		osg::Image* image = osgDB::readImageFile(tex.c_str());
+		if (image != NULL)
+		{
+			Textures[i] = new osg::Texture2D(image);
+			Textures[i]->setDataVariance(osg::Object::STATIC);		
+		}
+		else
+		{
+			printf("Could not load texture \"%s\"", tex.c_str());
+		}
 	}
 }
