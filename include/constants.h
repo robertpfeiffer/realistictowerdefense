0e5fd33ff69518b893d9ea27d123bc75e9483efe
Heiko Thiel <heiko.thiel@student.hpi.uni-potsdam.de>
2011-01-30 18:47:27 +0100

    Removed some constant, which are not anymore needed (because of INI -> XML)

--- include/constants.h
+++ include/constants.h
@@ -11,32 +11,3 @@
 
 #define MAP_DIRECTORY_TEXTURES "textures/"
 #define MAP_DIRECTORY_MODELS   "models/"
-
-#define MAP_SECTION_CHECKPOINTS "Checkpoints"
-#define MAP_SECTION_FIELDS      "Fields"
-#define MAP_SECTION_MAP         "Map"
-#define MAP_SECTION_MODELS      "Models"
-#define MAP_SECTION_SIZE        "Size"
-#define MAP_SECTION_TEXTURES    "Textures"
-#define MAP_SECTION_WORLD       "World"
-
-#define MAP_KEY_CHECKPOINTS_POINT_X_MASK  "Point%d.x"
-#define MAP_KEY_CHECKPOINTS_POINT_Y_MASK  "Point%d.y"
-
-#define MAP_KEY_FIELD_BUILDABLE  "Buildable"
-#define MAP_KEY_FIELD_ACCESSIBLE "Accessible"
-#define MAP_KEY_FIELD_TEXTUREID  "TextureId"
-#define MAP_KEY_FIELD_MODELID    "ModelId"
-
-#define MAP_KEY_FIELDS_PREFIX "Field_"
-
-#define MAP_KEY_MAP_ROW_MASK  "Row%d"
-
-#define MAP_KEY_MODEL_MASK    "Model%d"
-
-#define MAP_KEY_SIZE_WIDTH    "Width"
-#define MAP_KEY_SIZE_HEIGHT   "Height"
-
-#define MAP_KEY_TEXTURE_MASK  "Tex%d"
-
-#define MAP_KEY_WORLD_STRATA  "StrataTextureId"
