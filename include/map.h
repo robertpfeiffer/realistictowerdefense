9dfd237678d0f598b65e09a39677cee8fa2381d5
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2011-01-31 12:32:27 +0100

    fix for unix

--- include/map.h
+++ include/map.h
@@ -2,7 +2,7 @@
 #include <vector>
 #include <SimpleIni.h>
 #include <osg/Texture2D>
-
+#define MAXBYTE 0xff
 #include <field_node.h>
 
 class Map
@@ -23,9 +23,9 @@ class Map
 		long _height;
 		long _width;
 		std::vector<std::string> _fields;
-		std::vector<osg::ref_ptr<osg::Texture2D>> _textures;
-		std::vector<osg::ref_ptr<osg::Node>> _models;
+		std::vector<osg::ref_ptr< osg::Texture2D> > _textures;
+		std::vector<osg::ref_ptr< osg::Node> > _models;
 		CSimpleIni _ini;
 		osg::ref_ptr<FieldNode> _fieldBlocks[MAXBYTE];
-		std::vector<std::vector<long>> _checkpoints;
+		std::vector< std::vector<long> > _checkpoints;
 };
