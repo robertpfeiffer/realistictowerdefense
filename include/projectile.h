a2d8c31da93646468c919f4d2a5ae65dac566c27
Jan Sandbrink <fischikowski@web.de>
2011-02-28 17:49:06 +0100

    little work on projectile

--- include/projectile.h
+++ include/projectile.h
@@ -1,9 +1,14 @@
+#include <osg/Geometry>
+
 class ProjectileAttributes;
+class Creep;
 
 class Projectile
 {
 public:
-	
+	Projectile(osg::Vec3 origin, Creep* target, ProjectileAttributes* attributes);
+
 private:
 	ProjectileAttributes* _attributes;
+	Creep* _target;
 };
