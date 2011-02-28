b95e126b2f885fe9d788a0377ff9717c9db1f951
Jan Sandbrink <fischikowski@web.de>
2011-02-28 18:14:03 +0100

    more architecture for projectile

--- include/projectile.h
+++ include/projectile.h
@@ -1,14 +1,20 @@
 #include <osg/Geometry>
+#include <osg/PositionAttitudeTransform>
 
 class ProjectileAttributes;
 class Creep;
 
-class Projectile
+class Projectile : public osg::PositionAttitudeTransform
 {
 public:
 	Projectile(osg::Vec3 origin, Creep* target, ProjectileAttributes* attributes);
 
+	void onUpdate();
+
 private:
+	void hitTarget();
+	void approachToTarget();
+
 	ProjectileAttributes* _attributes;
 	Creep* _target;
 };
--- src/projectile.cpp
+++ src/projectile.cpp
@@ -1,1 +1,28 @@
 #include<projectile.h>
+
+Projectile::Projectile(osg::Vec3 origin, Creep* target, ProjectileAttributes* attributes)
+{
+	this->setPosition(origin);
+	_attributes = attributes;
+	_target = target;
+}
+
+void Projectile::onUpdate()
+{
+	approachToTarget();
+
+	if(/*close enough*/ false)
+	{
+		hitTarget();
+	}
+}
+
+void Projectile::hitTarget()
+{
+
+}
+
+void Projectile::approachToTarget()
+{
+
+}
