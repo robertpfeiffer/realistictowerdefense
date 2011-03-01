136a01e638b8fdfbaabf1e8f184e0f73474f7858
Jan Sandbrink <fischikowski@web.de>
2011-03-01 18:11:26 +0100

    small changes to field

--- src/field.cpp
+++ src/field.cpp
@@ -1,6 +1,7 @@
 // -*- mode: c++; coding: utf-8; c-basic-offset: 4; tab-width: 4; indent-tabs-mode:t; c-file-style: "stroustrup" -*-
 #include <field.h>
 #include <modeldata.h>
+#include <tower.h>
 #include <iostream>
 #include <stdlib.h>
 #include <math.h>
@@ -184,7 +185,7 @@ void Field::onBlur()
 
 bool Field::setBuilding()
 {
-	if (_fieldType != NULL && !_fieldType->isBuildable()) //FIXME
+	if (!_fieldType->isBuildable())
 	{
 	  	return false;
 	}
