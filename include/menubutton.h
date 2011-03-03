c05eb1bea066d47cdb08ecd39e5bfc6bff435216
Jan Sandbrink <fischikowski@web.de>
2011-03-03 16:07:45 +0100

    fieldmenubutton stubs

--- include/menubutton.h
+++ include/menubutton.h
@@ -1,11 +1,14 @@
 #pragma once
 #include <mouseeventhandler.h>
 #include <osg/Geometry>
+#include <osg/Texture2D>
 
 class MenuButton : public osg::Geometry
 {
 public:
 	MenuButton(const std::string texturepath);
+	MenuButton(osg::Texture2D* texture);
+	void init(osg::Texture2D* texture);
 	void setPosition(osg::Vec2 pos);
 	void setCallback(void (* callback)(osg::ref_ptr<MenuButton>));
 
@@ -14,6 +17,6 @@ public:
 private:
 	void (* _onClick)(osg::ref_ptr<MenuButton> );
 
-	osg::StateSet* createStateSetFromTexturePath(const std::string texturepath);
+	osg::StateSet* createStateSetFromTexture(osg::Texture2D* texture);
 };
 
