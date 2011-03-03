aed598cd02e9c49bf38d4a2ba37c19630c2a7237
Jan Sandbrink <fischikowski@web.de>
2011-03-03 17:57:26 +0100

    think i've finished the upgradebutton

--- include/towerupgradebutton.h
+++ include/towerupgradebutton.h
@@ -1,8 +1,16 @@
 #include <menubutton.h>
 
 class Tower;
+class TowerAttributes;
 
 class TowerUpgradeButton : public MenuButton
 {
+public:
+	TowerUpgradeButton(Tower* t, TowerAttributes* ta);
 
+	virtual void onClick(osgGA::GUIActionAdapter& aa);
+
+private:
+	Tower* _tower;
+	TowerAttributes* _towerattributes;
 };
