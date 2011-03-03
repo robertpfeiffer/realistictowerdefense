2c0556ddd9a25b392787073e483dfb48839f1cf7
Robert Pfeiffer <pfeiffer.robert@gmail.com>
2011-03-03 03:51:36 +0100

    hacky particle effect

--- src/field.cpp
+++ src/field.cpp
@@ -12,6 +12,24 @@
 #include <constants.h>
 #include <contextmenu.h>
 
+#include <osgParticle/SmokeEffect>
+#include <osgParticle/FireEffect>
+#include <osg/Geode>
+#include <osg/ShapeDrawable>
+
+#include <osgParticle/Particle>
+#include <osgParticle/ParticleSystem>
+#include <osgParticle/ParticleSystemUpdater>
+#include <osgParticle/ModularEmitter>
+#include <osgParticle/ModularProgram>
+#include <osgParticle/RandomRateCounter>
+#include <osgParticle/SectorPlacer>
+#include <osgParticle/RadialShooter>
+#include <osgParticle/AccelOperator>
+#include <osgParticle/FluidFrictionOperator>
+
+
+
 Field::Field(FieldType* fieldType) : _isBuildable(fieldType->isBuildable()), _ground(fieldType->getGround()), _fieldType(fieldType)
 {
 	this->addChild(_ground.get());
@@ -52,6 +70,98 @@ void build_tower(osg::ref_ptr<MenuButton> button)
 	}
 }
 
+void fire_powerup(osg::ref_ptr<MenuButton> button)
+{
+	Field* f = (dynamic_cast<Field*> (button->getParent(0)->getParent(0)));
+	//Tower* t = (dynamic_cast<Tower*> (f->getContent()));
+
+	if(f != NULL){
+
+    // Ok folks, this is the first particle system we build; it will be
+    // very simple, with no textures and no special effects, just default
+    // values except for a couple of attributes.
+
+    // First of all, we create the ParticleSystem object; it will hold
+    // our particles and expose the interface for managing them; this object
+    // is a Drawable, so we'll have to add it to a Geode later.
+
+		osgParticle::ParticleSystem *ps = new osgParticle::ParticleSystem;
+
+    // As for other Drawable classes, the aspect of graphical elements of
+    // ParticleSystem (the particles) depends on the StateAttribute's we
+    // give it. The ParticleSystem class has an helper function that let
+    // us specify a set of the most common attributes: setDefaultAttributes().
+    // This method can accept up to three parameters; the first is a texture
+    // name (std::string), which can be empty to disable texturing, the second
+    // sets whether particles have to be "emissive" (additive blending) or not;
+    // the third parameter enables or disables lighting.
+
+		ps->setDefaultAttributes("", true, false);
+
+    // Now that our particle system is set we have to create an emitter, that is
+    // an object (actually a Node descendant) that generate new particles at 
+    // each frame. The best choice is to use a ModularEmitter, which allow us to
+    // achieve a wide variety of emitting styles by composing the emitter using
+    // three objects: a "counter", a "placer" and a "shooter". The counter must
+    // tell the ModularEmitter how many particles it has to create for the
+    // current frame; then, the ModularEmitter creates these particles, and for
+    // each new particle it instructs the placer and the shooter to set its
+    // position vector and its velocity vector, respectively.
+    // By default, a ModularEmitter object initializes itself with a counter of
+    // type RandomRateCounter, a placer of type PointPlacer and a shooter of
+    // type RadialShooter (see documentation for details). We are going to leave
+    // these default objects there, but we'll modify the counter so that it
+    // counts faster (more particles are emitted at each frame).
+
+		osgParticle::ModularEmitter *emitter = new osgParticle::ModularEmitter;
+
+    // the first thing you *MUST* do after creating an emitter is to set the
+    // destination particle system, otherwise it won't know where to create
+    // new particles.
+
+		emitter->setParticleSystem(ps);
+	
+    // Ok, get a pointer to the emitter's Counter object. We could also
+    // create a new RandomRateCounter object and assign it to the emitter,
+    // but since the default counter is already a RandomRateCounter, we
+    // just get a pointer to it and change a value.
+
+		osgParticle::RandomRateCounter *rrc = 
+			static_cast<osgParticle::RandomRateCounter *>(emitter->getCounter());
+
+    // Now set the rate range to a better value. The actual rate at each frame
+    // will be chosen randomly within that range.
+
+		rrc->setRateRange(20, 30);    // generate 20 to 30 particles per second
+
+    // The emitter is done! Let's add it to the scene graph. The cool thing is
+    // that any emitter node will take into account the accumulated local-to-world
+    // matrix, so you can attach an emitter to a transform node and see it move.
+
+		f->addChild(emitter);
+
+    // Ok folks, we have almost finished. We don't add any particle modifier 
+    // here (see ModularProgram and Operator classes), so all we still need is
+    // to create a Geode and add the particle system to it, so it can be
+    // displayed.
+
+		osg::Geode *geode = new osg::Geode;    
+		geode->addDrawable(ps);
+
+    // add the geode to the scene graph
+
+		f->addChild(geode);
+/*
+        osgParticle::SmokeEffect* fire = new osgParticle::SmokeEffect(osg::Vec3(20.0f,20.0f,20.0f), 5.0f, 5.0f);
+		fire->setWind(osg::Vec3(1.0f,1.0f,0.0f));
+		f->addChild(fire);
+
+        osg::Geode* geode = new osg::Geode;
+        geode->addDrawable(fire->getParticleSystem());
+		f->addChild(geode);*/
+	}
+}
+
 void Field::onFocus(osgGA::GUIActionAdapter& aa)
 {
 	Contextmenu* aMenu= new Contextmenu();
@@ -62,8 +172,8 @@ void Field::onFocus(osgGA::GUIActionAdapter& aa)
 	}
 
 	//if (hasTower()){
-	//aMenu->addEntry(fire_powerup,
-	//				"textures/fire.png");
+	aMenu->addEntry(fire_powerup,
+					"textures/fire.png");
 	//}
 
 	aMenu->addEntry(NULL,
