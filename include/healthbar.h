7a4e53a58195abf12b3b2cd455b58519f8c581b9
Heiko Thiel <heiko.thiel@student.hpi.uni-potsdam.de>
2011-03-05 16:06:24 +0100

    Damage is now displayed short

--- include/healthbar.h
+++ include/healthbar.h
@@ -3,23 +3,29 @@
 
 #include <osg/Billboard>
 #include <osg/Drawable>
+#include <osg/Texture2D>
 #include <constants.h>
+#include <updatablenode.h>
 
-class HealthBar : public osg::Billboard
+class HealthBar : public osg::Billboard, public UpdatableNode
 {
 public:
 	HealthBar();
+	void onUpdate();
 	void setHealth(int health);
 	void setMaxHealth(int health, int maxHealth);
 private:
 	void _setBillBoardStateSet();
-	osg::Drawable* _createHealthGemoetry();
-	osg::Drawable* _createBackgroundGemoetry();
+	osg::Geometry* _createHealthGemoetry(osg::Texture2D* texture) const;
+	osg::Geometry* _createBackgroundGemoetry() const;
 	void _updateHealthBar();
-	void _updateTextureSize(osg::Geometry* geometry, int health) const;
+	void _updateTextureSize(osg::Geometry* geometry, int health, float textureOffset) const;
 	void _updateGeometrySize(osg::Geometry* geometry, float offset, float length, float height) const;
 	int _maxHealth;
 	int _health;
 	int _damage;
+	float _elapsedTimeSinceLastDamage;
+	int _damageLeft();
 	osg::ref_ptr<osg::Geometry> _healthGeometry;
+	osg::ref_ptr<osg::Geometry> _damageGeometry;
 };
--- src/healthbar.cpp
+++ src/healthbar.cpp
@@ -1,12 +1,16 @@
 // -*- mode: c++; coding: utf-8; c-basic-offset: 4; tab-width: 4; indent-tabs-mode: t; c-file-style: "stroustrup" -*-
 
-#include <healthbar.h>
 #include <sstream>
 #include <osg/BlendFunc>
 #include <osg/Geometry>
 #include <osg/AlphaFunc>
 #include <osg/Texture2D>
+
+#include <constants.h>
+#include <healthbar.h>
 #include <assetlibrary.h>
+#include <world.h>
+#include <gametimer.h>
 
 HealthBar::HealthBar()
 {
@@ -15,8 +19,39 @@ HealthBar::HealthBar()
 	_health = 1;
 	_maxHealth = 1;
 	this->_setBillBoardStateSet();
+
 	//this->addDrawable(_createBackgroundGemoetry());
-	this->addDrawable(_createHealthGemoetry());
+
+	_healthGeometry = _createHealthGemoetry(AssetLibrary::instance()->getTexture("healthbar/hp.png"));
+	this->addDrawable(_healthGeometry);
+
+	_damageGeometry = _createHealthGemoetry(AssetLibrary::instance()->getTexture("healthbar/damage.png"));
+	this->addDrawable(_damageGeometry);
+
+	_updateHealthBar();
+	_elapsedTimeSinceLastDamage = 0;
+	World::instance()->registerForUpdates(this);
+}
+
+void HealthBar::onUpdate()
+{
+	if (_damage == 0) return;
+
+	_elapsedTimeSinceLastDamage += GameTimer::instance()->elapsedTime();
+
+	if (_elapsedTimeSinceLastDamage > DAMAGE_DISPLAY_DURATION)
+	{
+		_damage = 0;
+	}
+
+	_updateHealthBar();
+}
+
+int HealthBar::_damageLeft()
+{
+	if (_elapsedTimeSinceLastDamage < DAMAGE_DISPLAY_DURATION) return _damage;
+
+	return _damage * (DAMAGE_DISPLAY_DECREASE_DURATION - (_elapsedTimeSinceLastDamage - DAMAGE_DISPLAY_DURATION)) / DAMAGE_DISPLAY_DECREASE_DURATION;
 }
 
 void HealthBar::_setBillBoardStateSet()
@@ -30,17 +65,17 @@ void HealthBar::_setBillBoardStateSet()
 	this->setStateSet(billBoardStateSet);
 }
 
-osg::Drawable* HealthBar::_createBackgroundGemoetry()
+osg::Geometry* HealthBar::_createBackgroundGemoetry() const
 {
 	float width = 0.8f;
 	float height = 0.12f;
 
-	osg::Geometry* backgroundGeometry = new osg::Geometry();
+	osg::Geometry* geometry = new osg::Geometry();
 
 	osg::Texture2D* backgroundTexture = AssetLibrary::instance()->getTexture("healthbar/background.png");
 	
 	// Declare and initialize geometry
-    backgroundGeometry = new osg::Geometry();
+    geometry = new osg::Geometry();
 
 	//add vertices and texture
 	osg::Vec3Array* verts = new osg::Vec3Array(4);	
@@ -48,7 +83,7 @@ osg::Drawable* HealthBar::_createBackgroundGemoetry()
 	(*verts)[1] = osg::Vec3( +width/2, 0, -height/2);
 	(*verts)[2] = osg::Vec3( +width/2, 0, +height/2);
 	(*verts)[3] = osg::Vec3( -width/2, 0, +height/2);
-	backgroundGeometry->setVertexArray(verts);
+	geometry->setVertexArray(verts);
 
 	
 	osg::Vec2Array* texCoords = new osg::Vec2Array(4);
@@ -56,47 +91,45 @@ osg::Drawable* HealthBar::_createBackgroundGemoetry()
 	(*texCoords)[1].set(1.0f, 0.0f);
 	(*texCoords)[2].set(1.0f, 1.0f);
 	(*texCoords)[3].set(0.0f, 1.0f);
-	backgroundGeometry->setTexCoordArray(0, texCoords);
+	geometry->setTexCoordArray(0, texCoords);
 
 	osg::StateSet* stateSet = new osg::StateSet();
 	stateSet->setTextureAttributeAndModes(0, backgroundTexture, osg::StateAttribute::ON);
-	backgroundGeometry->setStateSet(stateSet);
+	geometry->setStateSet(stateSet);
 
 	// Add a primitive set (QUADS) to the geometry
-	backgroundGeometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS,0,4));
+	geometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS,0,4));
 	
 	osg::Vec4Array* colors = new osg::Vec4Array;
 	colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	backgroundGeometry->setColorArray(colors);
-	backgroundGeometry->setColorBinding(osg::Geometry::BIND_OVERALL);
+	geometry->setColorArray(colors);
+	geometry->setColorBinding(osg::Geometry::BIND_OVERALL);
 	
-	return backgroundGeometry;
+	return geometry;
 }
 
-osg::Drawable* HealthBar::_createHealthGemoetry()
+osg::Geometry* HealthBar::_createHealthGemoetry(osg::Texture2D* texture) const
 {
-	osg::Texture2D* healthTexture = AssetLibrary::instance()->getTexture("healthbar/hp.png");
-	
 	// Declare and initialize geometry
-    _healthGeometry = new osg::Geometry();
+    osg::Geometry* geometry = new osg::Geometry();
 
 	//add vertices and texture
-	_healthGeometry->setVertexArray(new osg::Vec3Array(4));	
-	_healthGeometry->setTexCoordArray(0, new osg::Vec2Array(4));
-	_updateHealthBar();
+	geometry->setVertexArray(new osg::Vec3Array(4));	
+	geometry->setTexCoordArray(0, new osg::Vec2Array(4));
+	
 	osg::StateSet* stateSet = new osg::StateSet();
-	stateSet->setTextureAttributeAndModes(0, healthTexture, osg::StateAttribute::ON);
-	_healthGeometry->setStateSet(stateSet);
+	stateSet->setTextureAttributeAndModes(0, texture, osg::StateAttribute::ON);
+	geometry->setStateSet(stateSet);
 
 	// Add a primitive set (QUADS) to the geometry
-	_healthGeometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS,0,4));
+	geometry->addPrimitiveSet(new osg::DrawArrays(osg::PrimitiveSet::QUADS,0,4));
 	
 	osg::Vec4Array* colors = new osg::Vec4Array;
 	colors->push_back(osg::Vec4(1.0f, 1.0f, 1.0f, 1.0f));
-	_healthGeometry->setColorArray(colors);
-	_healthGeometry->setColorBinding(osg::Geometry::BIND_OVERALL);
+	geometry->setColorArray(colors);
+	geometry->setColorBinding(osg::Geometry::BIND_OVERALL);
 	
-	return _healthGeometry;
+	return geometry;
 }
 
 void HealthBar::_updateHealthBar()
@@ -109,7 +142,11 @@ void HealthBar::_updateHealthBar()
 	float halfWidth = width/2;	
 
 	_updateGeometrySize(_healthGeometry, -halfWidth, healthWidth, height);
-	_updateTextureSize(_healthGeometry, _health);	
+	_updateTextureSize(_healthGeometry, _health, 0.0f);
+
+	float damageWidth = width * static_cast<float>(_damage) / static_cast<float>(_maxHealth);
+	_updateGeometrySize(_damageGeometry, -halfWidth + healthWidth, damageWidth, height);
+	_updateTextureSize(_damageGeometry, _damage, healthWidth);
 }
 
 void HealthBar::_updateGeometrySize(osg::Geometry* geometry, float offset, float length, float height) const
@@ -122,17 +159,17 @@ void HealthBar::_updateGeometrySize(osg::Geometry* geometry, float offset, float
 	(*verts)[1] = osg::Vec3( offset + length, 0,  halfHeight);
 	(*verts)[2] = osg::Vec3( offset, 0,  halfHeight);
 	(*verts)[3] = osg::Vec3( offset, 0, -halfHeight);
-	_healthGeometry->setVertexArray(verts);
+	geometry->setVertexArray(verts);
 }
 
-void HealthBar::_updateTextureSize(osg::Geometry* geometry, int health) const
+void HealthBar::_updateTextureSize(osg::Geometry* geometry, int health, float textureOffset) const
 {
 	//update Texture width (reuse array, because we know how much vertices it has)
 	osg::Vec2Array* texCoords = (osg::Vec2Array*) geometry->getTexCoordArray(0);	
-	(*texCoords)[0].set((float)health / 100.0f, 0.0f);
-	(*texCoords)[1].set((float)health / 100.0f, 1.0f);
-	(*texCoords)[2].set(0.0f, 1.0f);
-	(*texCoords)[3].set(0.0f, 0.0f);
+	(*texCoords)[0].set(textureOffset + (float)health / 100.0f, 0.0f);
+	(*texCoords)[1].set(textureOffset + (float)health / 100.0f, 1.0f);
+	(*texCoords)[2].set(textureOffset, 1.0f);
+	(*texCoords)[3].set(textureOffset, 0.0f);
 	geometry->setTexCoordArray(0, texCoords);
 }
 
@@ -140,7 +177,8 @@ void HealthBar::setHealth(int health)
 {
 	if (health < _health)
 	{
-		_damage += _health - health;
+		_damage = _damageLeft() + (_health - health);
+		_elapsedTimeSinceLastDamage = 0;
 	}
 	_health = health;
 	_updateHealthBar();
