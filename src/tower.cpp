0ce94367fb5f08f06f5e0cbe3e66961001131b00
Jan Sandbrink <fischikowski@web.de>
2011-03-07 19:55:13 +0100

    removed tower-strategy code: like discussed

--- src/tower.cpp
+++ src/tower.cpp
@@ -5,7 +5,6 @@
 #include <gametimer.h>
 #include <hatchery.h>
 #include <hud.h>
-#include <field.h>
 #include <projectile.h>
 #include <towerattributes.h>
 #include <towercontextmenu.h>
@@ -31,7 +30,7 @@ void Tower::onUpdate()
 
 	_currentCooldown = _attributes->cooldown;
 
-	if(_target.get() == NULL || !_target->isAlive() || _target->isLeaked() || !isInRange(_target) || _attributes->strategy != 1)
+	if(_target.get() == NULL || !_target->isAlive() || _target->isLeaked() || !isInRange(_target))
 	{
 		if(!findNewTarget())
 		{
@@ -52,61 +51,24 @@ osg::Vec3 Tower::getPosition()
 {
 	return _position;
 }
-
 void Tower::upgradeTo(TowerAttributes* attributes)
 {
 	this->removeChild(_attributes->model);
 	this->addChild(attributes->model);
 	_attributes = attributes;
-	dynamic_cast<Field*>(this->getParent(0))->reset();
 }
 
 bool Tower::findNewTarget()
 {
 	float range = _attributes->range * _attributes->range; //we will do comparison on range 
 	std::set< osg::ref_ptr<Creep> >::iterator it;
-	_target = NULL;
-	switch(_attributes->strategy)
+	for(it = World::instance()->getCreepsIterator(); it != World::instance()->getCreepsIteratorEnd(); it++)
 	{
-	case 2:
-	{
-		//hit weakest target
-		int health = 1000000;
-		for(it = World::instance()->getCreepsIterator(); it != World::instance()->getCreepsIteratorEnd(); it++)
+		if(isInRange(*it))
 		{
-			if(isInRange(*it) && health >_target->health() )
-			{
-				_target = it->get();
-				health = _target->health();
-			}
-		}
-		if ( _target != NULL )
+			_target = it->get();
 			return true;
-	}
-	case 3:
-	{
-		//hit youngest target
-		for(it = World::instance()->getCreepsIterator(); it != World::instance()->getCreepsIteratorEnd(); it++)
-		{
-			if(isInRange(*it))
-			{
-				_target = it->get();
-			}
 		}
-		if ( _target != NULL )
-			return true;
-	}
-	default:
-	{
-		for(it = World::instance()->getCreepsIterator(); it != World::instance()->getCreepsIteratorEnd(); it++)
-		{
-			if(isInRange(*it))
-			{
-				_target = it->get();
-				return true;
-			}
-		}
-	}
 	}
 	_target = NULL;
 	return false;
